95fdcd0cb79c8e8b0bb77bbe32dc48b0b653dca6
adikumar8150 <adikumar8150@gmail.com>
2024-12-17 12:12:14 +0530

    meal plan has been set up dynamically

--- src/components/MealCard.jsx
+++ src/components/MealCard.jsx
@@ -1,32 +1,24 @@
-function MealCard({ calorieRange, meal }) {
+function MealCard({ meal }) {
+  if (!meal) return <p>No meal plan available for the selected preference.</p>;
+
+  console.log("meal quantites", meal.quantity);
   return (
     <div className="p-4 border rounded">
-      <h2 className="font-bold text-lg">{calorieRange} Plan</h2>
-
+      <h2 className="font-bold text-lg">{meal.item}</h2>
       <p>
-        <strong>Breakfast:</strong> {meal.breakfast.item} (
-        {meal.breakfast.quantity})
-        <span> protein: {meal?.breakfast?.protein}</span>
-        <span> carbs: {meal?.breakfast?.carbs}</span>
-        <span> fats: {meal?.breakfast?.fats}</span>
+        <strong>Quantity:</strong>
+        {Object.entries(meal?.quantity).map(([ingredient, quantity], index) => (
+          <li key={index}>
+            {ingredient}: {quantity}
+          </li>
+        ))}
       </p>
       <p>
-        <strong>Lunch:</strong> {meal.lunch.item} ({meal.lunch.quantity})
-        <span> protein: {meal?.lunch?.protein}</span>
-        <span> carbs: {meal?.lunch?.carbs}</span>
-        <span> fats: {meal?.lunch?.fats}</span>
+        <strong>Protein:</strong> {meal.protein}g | <strong>Carbs:</strong>{" "}
+        {meal.carbs}g | <strong>Fats:</strong> {meal.fats}g
       </p>
       <p>
-        <strong>Dinner:</strong> {meal.dinner.item} ({meal.dinner.quantity})
-        <span> protein: {meal?.dinner?.protein}</span>
-        <span> carbs: {meal?.dinner?.carbs}</span>
-        <span> fats: {meal?.dinner?.fats}</span>
-      </p>
-      <p>
-        <strong>Snack:</strong> {meal.snack.item} ({meal.snack.quantity})
-        <span> protein: {meal?.snack?.protein}</span>
-        <span> carbs: {meal?.snack?.carbs}</span>
-        <span> fats: {meal?.snack?.fats}</span>
+        <strong>Calories:</strong> {meal.calories} kcal
       </p>
     </div>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,6 @@
 import MealPlanForm from "../components/MealplanForm";
 
 export default function HomePage() {
-  // function to find the closest meal plan with the deficit in the meal.json
   return (
     <>
       <div className="main-container">
